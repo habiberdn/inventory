064ce05c2efb24bb4083bf51eb62a414df2cb7ed
Habib <habib.erdian@gmail.com>
2024-08-31 23:22:09 +0700

    change react route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import AddProduct from './pages/add_product';
+import AddCategory from './pages/add_categories';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<AddProduct />} />
+        <Route path="/new/products" element={<AddProduct />} />
+        <Route path="/new/categories" element={<AddCategory/>} />
       </Routes>
     </Router>
   )
