f5b01c73c2cdfb2b2875d82e92fff88ec81d7201
Habib <habib.erdian@gmail.com>
2024-09-06 15:01:26 +0700

    added save button on add product file

--- client/src/pages/add_product.tsx
+++ client/src/pages/add_product.tsx
@@ -13,7 +13,13 @@ function AddProduct() {
           <Navbar />
           <div className="p-4 w-full flex flex-col gap-4 overflow-auto">
             <ProductInfo />
-            <PenjualanInfo/>
+            <PenjualanInfo />
+            <div className="p-2 bg-white flex flex-col rounded-lg gap-2">
+              <div className="flex  gap-4 justify-end p-2 mr-[1rem]">
+                <button className="border p-2 px-4 rounded-lg bg-[#21263c] text-white">Save</button>
+                <button className="border p-2 rounded-lg">Cancel</button>
+              </div>
+            </div>
           </div>
         </div>
       </div>
