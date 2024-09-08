6d705d9cc8b03b0bcd8a78842c65d26b86b7bdcd
Habib <habib.erdian@gmail.com>
2024-09-08 19:30:03 +0700

    change input interface and default input state

--- client/src/pages/add_product.tsx
+++ client/src/pages/add_product.tsx
@@ -2,9 +2,42 @@ import ProductInfo from "../components/product_info";
 import Navbar from "../components/navbar";
 import Sidebar from "../components/sidebar";
 import PenjualanInfo from "../components/penjualan_info";
+import { useState } from "react";
+
+interface Input {
+  name?: string;
+  description?: string;
+  category_name?: string;
+  price?: number[][];
+  amount?: number[][];
+  variantName?: string[];
+  variantValue?: string[][];
+  codeVariant?: string[][]
+  parentId?: number;
+}
 
 function AddProduct() {
+  const [input, setInput] = useState<Input>({
+    name: "",
+    description: "",
+    category_name: "",
+    price: [],
+    amount: [],
+    codeVariant: [],
+    variantName: [],
+    variantValue: [[]],
+    parentId: 0
+  })
 
+  const getValue = (input: Input) => {
+    console.log(input)
+    setInput((prev) => ({
+      ...prev,
+      ...input,
+    }));
+  };
+
+  console.log(input)
   return (
     <main className="flex flex-col h-screen bg-[#f7f7fe]">
       <div className="flex h-full">
@@ -12,8 +45,8 @@ function AddProduct() {
         <div className="flex flex-col w-full gap-4 h-full overflow-hidden">
           <Navbar />
           <div className="p-4 w-full flex flex-col gap-4 overflow-auto">
-            <ProductInfo />
-            <PenjualanInfo />
+            <ProductInfo getValue={getValue} />
+            <PenjualanInfo getValue={getValue} />
             <div className="p-2 bg-white flex flex-col rounded-lg gap-2">
               <div className="flex  gap-4 justify-end p-2 mr-[1rem]">
                 <button className="border p-2 px-4 rounded-lg bg-[#21263c] text-white">Save</button>
