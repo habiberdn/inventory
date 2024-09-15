f6752ea9710de61e2fee6b55c010c4d398b877ec
Habib <habib.erdian@gmail.com>
2024-09-15 20:29:19 +0700

    modified a createProduct controller

--- backend/controller/productController.ts
+++ backend/controller/productController.ts
@@ -1,5 +1,5 @@
 import { PrismaClient } from "@prisma/client";
-import { AppError } from "../utils/appError";
+import { addProductSchema } from "../utils/validateSchema";
 
 const prisma = new PrismaClient();
 
@@ -11,19 +11,62 @@ const corsHeaders = {
 
 const createProduct = async (req: Request): Promise<Response> => {
     try {
-        const body = await req.json();
-        const { name, description,category_name,amount, } = body;
+        const body = await addProductSchema.parseAsync(await req.json())
+
+        const category_length = body.category_name.split(" > ").length
+        const category = body.category_name.split(" > ")[category_length - 1]
 
         const product = await prisma.product.create({
             data: {
-                name,
-                description,
-                category_name,
-
+                name: body.name,
+                description: body.description,
+                category_name: category,
             },
         });
 
-        return new Response(JSON.stringify(product), {
+        const newVariant = await prisma.variant.create({
+            data: {
+                variantName: body.variantName[0],
+                product_name: product.name,
+                secondVariant: body.variantName[1]
+            },
+        });
+
+        const firstVariantValues = body.variantValue[0].slice(0, -1).map(async (value1) => {
+
+            return prisma.variantValue.create({
+                data: {
+                    variantValue: value1,
+                    variantNameId: newVariant.id,
+                },
+            });
+        })
+
+        const secondVariantValues = body.variantValue[1].slice(0, -1).map(async (value1) => {
+
+            return prisma.secondVariantValue.create({
+                data: {
+                    variantValue: value1,
+                    VariantId: newVariant.id
+                },
+            });
+        })
+
+        const stockEntries = body.price.map(async (value, index) => {
+            return prisma.stock.create({
+                data: {
+                    price: value,
+                    amount: body.amount[index],
+                    codeVariant: body.codeVariant[index],
+                    VariantId: newVariant.id
+                }
+            })
+        })
+
+        await Promise.all([...firstVariantValues, ...secondVariantValues, ...stockEntries]);
+
+        console.log(body)
+        return new Response(JSON.stringify("Product Created!"), {
             status: 200,
             headers: { 'Content-Type': 'application/json', ...corsHeaders },
         });
@@ -52,68 +95,5 @@ const getProducts = async (req: Request): Promise<Response> => {
     }
 };
 
-// const getProductById: MiddlewareProduct = async (req) => {
-// try {
-// const product = await prisma.product.findUnique({
-//     where: { id: parseInt(req.body.id, 10) },
-// });
-
-// if (!product) {
-//     res.statusCode = 404;
-//     res.end('Product not found');
-//     return;
-// }
-
-// res.statusCode = 200;
-// res.setHeader('Content-Type', 'application/json');
-// res.end(JSON.stringify(product));
-// } catch (error) {
-//     return new Response(JSON.stringify({ error: 'Failed to fetch products' }), {
-//         status: 500, // 500 for an internal server error
-//         headers: { 'Content-Type': 'application/json' },
-//     });
-// }
-// }
-
-// async function updateProduct(req: IncomingMessage, res: ServerResponse) {
-//     try {
-
-//         const { id, name, description, status } = JSON.parse(body);
-
-//         const product = await prisma.product.update({
-//             where: { id: parseInt(id, 10) },
-//             data: {
-//                 name,
-//                 description,
-//                 status,
-//             },
-//         });
-
-//         res.statusCode = 200;
-//         res.setHeader('Content-Type', 'application/json');
-//         res.end(JSON.stringify(product));
-//     } catch (error) {
-// return new Response(JSON.stringify({ error: 'Failed to update products' }), {
-//     status: 500, // 500 for an internal server error
-//     headers: { 'Content-Type': 'application/json' },
-//   });
-//     }
-// }
-
-// async function deleteProduct(req: IncomingMessage, res: ServerResponse, id: string) {
-//     try {
-//         await prisma.product.delete({
-//             where: { id: parseInt(id, 10) },
-//         });
-
-//         res.statusCode = 204;
-//         res.end();
-//     } catch (error) {
-// return new Response(JSON.stringify({ error: 'Failed to delete products' }), {
-//     status: 500, // 500 for an internal server error
-//     headers: { 'Content-Type': 'application/json' },
-//   });
-//     }
-// }
 
 export default { createProduct, getProducts }
