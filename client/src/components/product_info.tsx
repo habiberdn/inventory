aaca96f72498e3bdc3ca22db6690bfc953fc11d1
Habib <habib.erdian@gmail.com>
2024-09-16 18:06:10 +0700

    modified product_info file for react form hook

--- client/src/components/product_info.tsx
+++ client/src/components/product_info.tsx
@@ -3,6 +3,17 @@ import axios from "axios";
 import { Input } from "../app/ui/input";
 import { Textarea } from "../app/ui/textarea";
 import Modal from "./modal";
+import { Controller, UseFormReturn } from "react-hook-form";
+import FormSchema from "@/lib/formSchema";
+import { z } from "zod"
+import {
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/app/ui/form"
+import { Button } from "@/app/ui/button";
 
 interface Product {
     id: number;
@@ -31,22 +42,24 @@ interface Input {
     variantValue?: string[][];
     codeVariant?: string[]
     parentId?: number;
-  }
+}
 
 interface Props {
     getValue: (props: Input) => void;
+    form: UseFormReturn<z.infer<typeof FormSchema>>;
+    input: Input
 }
 
-const ProductInfo = ({ getValue }: Props) => {
+const ProductInfo = ({ getValue, form, input }: Props) => {
     const [digit, setDigit] = useState(0);
     const [isClick, setClick] = useState(false);
     const [category, setCategory] = useState<Category[]>([]);
-    const [value, setValue] = useState<Product>({
+    const [value, setValueProduct] = useState<Product>({
         id: 0,
         name: "",
         description: "",
         category_name: "",
-        price: 0,  
+        price: 0,
         status: false,
     });
 
@@ -63,33 +76,17 @@ const ProductInfo = ({ getValue }: Props) => {
     useEffect(() => {
         getValue({
             ...value,
-            price: [value.price], // Convert string to number
-        }); 
+            price: [value.price],
+        });
     }, [value]); // Add value as a dependency
 
     function addFlag(productCategory: string) {
-        setValue((prev) => ({
+        form.setValue('category_name', productCategory);
+        setValueProduct((prev) => ({
             ...prev,
             category_name: productCategory,
         }));
     }
-
-    const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
-        const { value, name } = e.target;
-        setDigit(value.length);
-        setValue((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
-
-    const handleChangeInput = (e: React.ChangeEvent<HTMLInputElement>) => {
-        const { value, name } = e.target;
-        setValue((prev) => ({
-            ...prev,
-            [name]: value,
-        }));
-    };
 
     const handleClick: MouseEventHandler<HTMLInputElement> = () => {
         setClick(!isClick);
@@ -98,29 +95,88 @@ const ProductInfo = ({ getValue }: Props) => {
     const closeModal = () => {
         setClick(false);
     };
+    const handleTextAreaChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+        const { value, name } = e.target;
+        setDigit(value.length);
+        setValueProduct((prev) => ({
+            ...prev,
+            [name]: value,
+        }));
+    };
+
 
     return (
         <div className="p-6 bg-white flex flex-col rounded-lg gap-4">
             <p className="text-xl font-bold">Informasi Product</p>
             <div className="flex flex-col gap-2 justify-center p-4 ml-[1rem]">
                 <div className="grid w-[70%] items-center gap-1">
-                    <label htmlFor="Nama" className="text-sm">Nama Barang</label>
-                    <Input type="text" id="Nama" placeholder="Nama Barang" name="name" onChange={handleChangeInput} />
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-sm">Nama Barang</FormLabel>
+                                <FormControl>
+                                    <Input type="text" placeholder="Nama Barang"  {...field} />
+                                </FormControl>
+                                <FormMessage className="text-red-500 text-xs" />
+                            </FormItem>
+                        )}>
+
+                    </FormField>
                 </div>
-                <div className="grid w-[70%] items-center gap-1.5 z-20 h-[4rem]">
-                    <label htmlFor="Kategori" className="text-sm">Kategori</label>
-                    <Input type="text" id="Kategori" placeholder="Kategori" onClick={handleClick} name="category_name" onChange={handleChangeInput} value={value.category_name} />
-                    <div className="flex justify-center items-center">
-                        {isClick && <Modal isClick={isClick} closeModal={closeModal} category={category} getValue={addFlag} />}
-                    </div>
+                <div className="grid w-[70%] items-center gap-1.5 z-20 ">
+                    <FormField
+                        control={form.control}
+                        name="category_name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-sm">Kategori</FormLabel>
+                                <FormControl>
+                                    <Controller control={form.control} name="category_name" render={({ field }) => (
+                                        <Input
+                                            type="text"
+                                            {...field}
+                                            id="Kategori"
+                                            className="text-left"
+                                            placeholder="Kategori"
+                                            onChange={() => { field.onChange(input.category_name) }}
+                                            value={field.value || input.category_name}
+                                            onClick={handleClick} // Open the Modal on click
+                                        />
+
+                                    )} />
+                                </FormControl>
+                                <FormMessage className="text-red-500 text-xs" />
+                                <div className="flex justify-center items-center">
+                                    {isClick && <Modal isClick={isClick} closeModal={closeModal} category={category} getValue={addFlag} />}
+                                </div>
+                            </FormItem>
+                        )}>
+
+                    </FormField>
+
+
                 </div>
-                <div className="grid w-[70%] items-center gap-1.5">
-                    <label htmlFor="Deskripsi" className="text-sm">Deskripsi Barang</label>
-                    <Textarea id="Deskripsi" placeholder="Deskripsi Barang" onChange={handleTextAreaChange} maxLength={100} name="description" />
+                <div className="grid w-[70%] items-center gap-1.5 ">
+                    <FormField
+                        control={form.control}
+                        name="description"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-sm">Description</FormLabel>
+                                <FormControl>
+                                    <Textarea placeholder="Description" {...field} />
+                                </FormControl>
+                                <FormMessage className="text-red-500 text-xs" />
+                            </FormItem>
+
+                        )}>
+                    </FormField>
                     <p className="text-right text-[#8E8E8E] text-sm">{digit}/100</p>
                 </div>
             </div>
-        </div>
+        </div >
     );
 };
 
