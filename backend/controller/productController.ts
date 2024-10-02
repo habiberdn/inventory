38adc644420724a0a3917c809b6ed68e74ba0e9c
Habib <habib.erdian@gmail.com>
2024-10-02 19:30:56 +0700

    fixing productController code

--- backend/controller/productController.ts
+++ backend/controller/productController.ts
@@ -14,8 +14,8 @@ const createProduct = async (req: Request): Promise<Response> => {
         const body = await addProductSchema.parseAsync(await req.json())
 
         const category_length = body.category_name.split(" > ").length
-        const category = body.category_name.split(" > ")[category_length - 1]
-
+        const category = body.category_name.split(" > ") ? body.category_name.split(" > ")[category_length - 1] : body.category_name
+        console.log(category)
         const product = await prisma.product.create({
             data: {
                 name: body.name,
@@ -23,45 +23,60 @@ const createProduct = async (req: Request): Promise<Response> => {
                 category_name: category,
             },
         });
-
-        const newVariant = await prisma.variant.create({
-            data: {
-                variantName: body.variantName[0],
-                product_name: product.name,
-                secondVariant: body.variantName[1]
-            },
-        });
-
-        const firstVariantValues = body.variantValue[0].slice(0, -1).map(async (value1) => {
-
-            return prisma.variantValue.create({
+        let newVariant: any;
+        if (body.variantName) {
+            newVariant = await prisma.variant.create({
                 data: {
-                    variantValue: value1,
-                    variantNameId: newVariant.id,
+                    variantName: body.variantName,
+                    product_name: product.name,
+                    secondVariant: body.secondVariant
                 },
             });
-        })
 
-        const secondVariantValues = body.variantValue[1].slice(0, -1).map(async (value1) => {
+        }
+        let firstVariantValues: any
+        if (body.variantValue && body.variantValue[0]) {
 
-            return prisma.secondVariantValue.create({
-                data: {
-                    variantValue: value1,
-                    VariantId: newVariant.id
-                },
-            });
-        })
+            firstVariantValues = body.variantValue[0].slice(0, -1).map(async (value1) => {
 
-        const stockEntries = body.price.map(async (value, index) => {
-            return prisma.stock.create({
-                data: {
-                    price: value,
-                    amount: body.amount[index],
-                    codeVariant: body.codeVariant[index],
-                    VariantId: newVariant.id
-                }
+                return prisma.variantValue.create({
+                    data: {
+                        variantValue: value1,
+                        variantNameId: newVariant.id,
+                    },
+                });
             })
-        })
+        }
+
+        let secondVariantValues: any
+        if (body.variantValue && body.variantValue[1]) {
+            secondVariantValues = body.variantValue[1].slice(0, -1).map(async (value1) => {
+
+                return prisma.secondVariantValue.create({
+                    data: {
+                        variantValue: value1,
+                        VariantId: newVariant.id
+                    },
+                });
+            })
+
+        }
+
+        let stockEntries: any   
+        if (body.price && body.amount  && body.codeVariant) {
+
+            stockEntries = body.price.map(async (value, index) => {
+                return prisma.stock.create({
+                    data: {
+                        price: value,
+                        amount:body.amount ? body.amount[index] : 0,
+                        codeVariant:body.codeVariant ? body.codeVariant[index]: "",
+                        VariantId: newVariant.id
+                    }
+                })  
+            })
+        }
+
 
         await Promise.all([...firstVariantValues, ...secondVariantValues, ...stockEntries]);
 
@@ -70,12 +85,22 @@ const createProduct = async (req: Request): Promise<Response> => {
             status: 200,
             headers: { 'Content-Type': 'application/json', ...corsHeaders },
         });
-    } catch (error) {
-        console.error('Error fetching products:', error);
-        return new Response(JSON.stringify({ error: 'Failed to fetch products' }), {
+    }
+    catch (error) {
+        if (error instanceof Error) {
+            console.error('Error creating product:', error);
+            return new Response(JSON.stringify({ error: "Error creating product" }), {
+                status: 500,
+                headers: { 'Content-Type': 'application/json', ...corsHeaders },
+            });
+        }
+
+        console.error('Unexpected error:', error);
+        return new Response(JSON.stringify({ error: 'An unknown error occurred' }), {
             status: 500,
-            headers: { 'Content-Type': 'application/json' },
+            headers: { 'Content-Type': 'application/json', ...corsHeaders },
         });
+
     }
 }
 
